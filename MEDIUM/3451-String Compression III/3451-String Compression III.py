d42ac7408859e886621f272ab13bb7c35728abdf
Hariprasad C <haripc525@gmail.com>
2025-04-25 17:52:41 +0530

    Add/update solution for String Compression III

---
+++ MEDIUM/3451-String Compression III/3451-String Compression III.py
@@ -0,0 +1,22 @@
+// Problem: String Compression III
+// Difficulty: MEDIUM
+// LeetCode URL: https://leetcode.com/problems/string-compression-iii/
+// Date: 2024-11-07
+
+class Solution:
+    def compressedString(self, word: str) -> str:
+        n = len(word)
+        count = 0
+        i = 0
+        j = 0
+        ans = ""
+        
+        while j < n:
+            count = 0
+            while j < n and word[i] == word[j] and count < 9:
+                j += 1
+                count += 1
+            ans += str(count) + word[i]
+            i = j
+        
+        return ans
