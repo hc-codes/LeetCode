9e6fc485aaf695b04879aa16a705628cccfcb1af
Hariprasad C <haripc525@gmail.com>
2025-04-25 17:50:42 +0530

    Add/update solution for Concatenation of Array

---
+++ EASY/2058-Concatenation of Array/2058-Concatenation of Array.py
@@ -0,0 +1,8 @@
+// Problem: Concatenation of Array
+// Difficulty: EASY
+// LeetCode URL: https://leetcode.com/problems/concatenation-of-array/
+// Date: 2024-09-01
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums+nums
