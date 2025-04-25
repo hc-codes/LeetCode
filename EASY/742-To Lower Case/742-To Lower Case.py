3194ef56539bbc22435e409bb2a0cbe5b882a157
Hariprasad C <haripc525@gmail.com>
2025-04-25 17:46:52 +0530

    Add/update solution for To Lower Case

---
+++ EASY/742-To Lower Case/742-To Lower Case.py
@@ -0,0 +1,8 @@
+// Problem: To Lower Case
+// Difficulty: EASY
+// LeetCode URL: https://leetcode.com/problems/to-lower-case/
+// Date: 2023-12-15
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
