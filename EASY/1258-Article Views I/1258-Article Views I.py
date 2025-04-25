31cc0192daa28a850340847909e40b207cbe131b
Hariprasad C <haripc525@gmail.com>
2025-04-25 17:48:14 +0530

    Add/update solution for Article Views I

---
+++ EASY/1258-Article Views I/1258-Article Views I.py
@@ -0,0 +1,7 @@
+// Problem: Article Views I
+// Difficulty: EASY
+// LeetCode URL: https://leetcode.com/problems/article-views-i/
+// Date: 2024-10-19
+
+/* Write your T-SQL query statement below */
+select distinct author_id as id  from views where author_id = viewer_id
