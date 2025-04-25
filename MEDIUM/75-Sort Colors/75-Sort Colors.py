47d6ae4d3e6612cdf273fa6f86ae058c5e2f628f
Hariprasad C <haripc525@gmail.com>
2025-04-25 17:41:46 +0530

    Add/update solution for Sort Colors

---
+++ MEDIUM/75-Sort Colors/75-Sort Colors.py
@@ -0,0 +1,24 @@
+// Problem: Sort Colors
+// Difficulty: MEDIUM
+// LeetCode URL: https://leetcode.com/problems/sort-colors/
+// Date: 2024-07-16
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        low=0
+        mid=0
+        high=len(nums)-1
+        while(mid<=high):
+            if(nums[mid]==0):
+                nums[mid],nums[low]=nums[low],nums[mid]
+                mid+=1
+                low+=1
+            elif(nums[mid]==1):
+                mid+=1
+            else:
+                nums[mid],nums[high]=nums[high],nums[mid]
+                high-=1
+                
