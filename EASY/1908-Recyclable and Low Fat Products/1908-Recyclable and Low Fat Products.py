650cf05ddb9c6dd982e0a1d9374ce93ddfe7cb04
Hariprasad C <haripc525@gmail.com>
2025-04-25 17:49:57 +0530

    Add/update solution for Recyclable and Low Fat Products

---
+++ EASY/1908-Recyclable and Low Fat Products/1908-Recyclable and Low Fat Products.py
@@ -0,0 +1,7 @@
+// Problem: Recyclable and Low Fat Products
+// Difficulty: EASY
+// LeetCode URL: https://leetcode.com/problems/recyclable-and-low-fat-products/
+// Date: 2024-10-19
+
+# Write your MySQL query statement below
+select product_id from products where low_fats='Y' and recyclable='Y'
