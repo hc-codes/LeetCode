6f7b177f0f350aa3d5fdcd4d8a47c03736051878
Hariprasad C <haripc525@gmail.com>
2025-05-31 14:57:22 +0530

    Create 424-LongestRepeatingCharacterReplacement

---
+++ 424-LongestRepeatingCharacterReplacement/version/python/424-LongestRepeatingCharacterReplacement_20250531_145722.py
@@ -0,0 +1,15 @@
+# Last updated: 5/31/2025, 2:57:22 PM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        if root:
+            res = self.postorderTraversal(root.left)
+            res = res + self.postorderTraversal(root.right)
+            res.append(root.val)
+        return res
